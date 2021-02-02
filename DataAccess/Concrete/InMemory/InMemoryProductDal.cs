6ca640614969f7e7f755f77c18b33e57d205469c
htcoztrk <haticeozturk0207@gmail.com>
2021-02-02 21:09:52 +0300

    initial commit

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product{ CategoryId=1, ProductId=1, ProductName="Bardak", UnitPrice=15, UnitsInStock=15  }
+                new Product{ CategoryId=2, ProductId=2, ProductName="Camera", UnitPrice=500, UnitsInStock=3  }
+                new Product{ CategoryId=2, ProductId=3, ProductName="Telefon", UnitPrice=1500, UnitsInStock=2  }
+                new Product{ CategoryId=2, ProductId=4, ProductName="Klavye", UnitPrice=150, UnitsInStock=65  }
+                new Product{ CategoryId=2, ProductId=5, ProductName="Bardak", UnitPrice=82, UnitsInStock=1  }
+            };
+        }
+        public void Add(Product prodct)
+        {
+            
+        }
+
+        public void Delete(Product product)
+        {
+           
+        }
+
+        public List<Product> GetAll()
+        {
+            
+        }
+
+        public void Update(Product product)
+        {
+          
+        }
+    }
+}
